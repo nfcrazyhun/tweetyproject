df527ad14559770ff6bfef4ce5085085da7d8ae9
Foldesi Peter <peter.foldesi@nevis.hu>
2020-08-17 18:45:47 +0200

    Update tweets index

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -10,11 +10,11 @@ class TweetController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function index()
     {
-        //
+        return view('tweets.index');
     }
 
     /**
