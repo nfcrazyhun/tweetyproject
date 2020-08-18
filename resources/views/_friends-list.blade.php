a1da1f91a475d43615af86f4f3610b579347abcd
Foldesi Peter <peter.foldesi@nevis.hu>
2020-08-18 19:16:35 +0200

    Update Following box

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,25 +1,25 @@
 <div class="p-3 border border-info rounded-lg">
     <h3 class="font-bold text-xl mb-4">Following</h3>
 
-    <ul>
-{{--        @forelse (auth()->user()->follows as $user)--}}
-{{--            <li class="{{ $loop->last ? '' : 'mb-4' }}">--}}
-{{--                <div>--}}
-{{--                    <a href="{{ $user->path() }}" class="flex items-center text-sm">--}}
-{{--                        <img--}}
-{{--                            src="{{ $user->avatar }}"--}}
-{{--                            alt=""--}}
-{{--                            class="rounded-full mr-2"--}}
-{{--                            width="40"--}}
-{{--                            height="40"--}}
-{{--                        >--}}
+    <ul class="list-unstyled">
+        @forelse (auth()->user()->follows as $user)
+            <li class="{{ $loop->last ? '' : 'mb-4' }}">
+                <div>
+                    <a href="{{ $user->path() }}" class="flex items-center text-sm">
+                        <img
+                            src="{{ $user->avatar }}"
+                            alt=""
+                            class="rounded-full mr-2"
+                            width="40"
+                            height="40"
+                        >
 
-{{--                        {{ $user->name }}--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--            </li>--}}
-{{--        @empty--}}
+                        {{ $user->name }}
+                    </a>
+                </div>
+            </li>
+        @empty
         <p class="my-4">No friends yet!</p>
-{{--        @endforelse--}}
+        @endforelse
     </ul>
 </div>
