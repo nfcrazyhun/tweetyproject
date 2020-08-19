2da0340e7e3aae16c4c933520a6994a4d19bf7bc
Foldesi Peter <peter.foldesi@nevis.hu>
2020-08-19 16:58:23 +0200

    Add views to profile

---
+++ resources/views/profiles/edit.blade.php
@@ -0,0 +1,205 @@
+<x-app>
+    <form method="POST" action="{{ $user->path() }}" enctype="multipart/form-data">
+        @csrf
+        @method('PATCH')
+
+        <div class="mb-6">
+            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                   for="name"
+            >
+                Name
+            </label>
+
+            <input class="border border-gray-400 p-2 w-full"
+                   type="text"
+                   name="name"
+                   id="name"
+                   value="{{ $user->name }}"
+                   required
+            >
+
+            @error('name')
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mb-6">
+            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                   for="username"
+            >
+                Username
+            </label>
+
+            <input class="border border-gray-400 p-2 w-full"
+                   type="text"
+                   name="username"
+                   id="username"
+                   value="{{ $user->username }}"
+                   required
+            >
+
+            @error('username')
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mb-6">
+            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                   for="description"
+            >
+                Description
+            </label>
+
+            <textarea
+                class="border border-gray-400 p-2 w-full"
+                name="description"
+                id="description"
+                rows="5"
+            >{{ $user->description }}</textarea>
+
+            @error('description')
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mb-6">
+            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                   for="avatar"
+            >
+                Avatar
+            </label>
+
+            <div class="flex">
+                <input class="border border-gray-400 p-2 w-full"
+                       type="file"
+                       name="avatar"
+                       id="avatar"
+                       accept="image/*"
+                >
+
+                <img src="{{ $user->avatar }}"
+                     alt="your avatar"
+                     width="40"
+                >
+
+                @if( false === strpos( $user->avatar, 'images/default-avatar' ) )
+                    <a href="{{ route('profile', $user ) . '/edit/remove/avatar' }}"
+                       class="bg-red-400 text-white rounded py-2 px-4 hover:bg-blue-500 ml-4 inline-block"
+                       style="line-height: 31px"
+                    >
+                        X
+                    </a>
+                @endif
+            </div>
+
+
+            @error('avatar')
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+
+
+        <div class="mb-6">
+            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                   for="avatar"
+            >
+                Banner
+            </label>
+
+            <div class="relative">
+                @if( false === strpos( $user->banner, 'images/default-profile-banner.jpg' ) )
+                    <a href="{{ route('profile', $user ) . '/edit/remove/banner' }}"
+                       class="bg-red-400 absolute text-white rounded py-2 px-4 hover:bg-blue-500 mr-4"
+                    >
+                        X
+                    </a>
+                @endif
+                <img src="{{ $user->banner }}"
+                     alt="your banner"
+                     class="w-auto mb-4"
+                     style="max-height:250px;max-width:100%"
+                >
+            </div>
+
+            <input class="border border-gray-400 p-2 w-full"
+                   type="file"
+                   name="banner"
+                   id="banner"
+                   accept="image/*"
+            >
+
+            @error('banner')
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+
+
+        <div class="mb-6">
+            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                   for="email"
+            >
+                Email
+            </label>
+
+            <input class="border border-gray-400 p-2 w-full"
+                   type="email"
+                   name="email"
+                   id="email"
+                   value="{{ $user->email }}"
+                   required
+            >
+
+            @error('email')
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mb-6">
+            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                   for="password"
+            >
+                Password
+            </label>
+
+            <input class="border border-gray-400 p-2 w-full"
+                   type="password"
+                   name="password"
+                   id="password"
+                   required
+            >
+
+            @error('password')
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mb-6">
+            <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                   for="password_confirmation"
+            >
+                Password Confirmation
+            </label>
+
+            <input class="border border-gray-400 p-2 w-full"
+                   type="password"
+                   name="password_confirmation"
+                   id="password_confirmation"
+                   required
+            >
+
+            @error('password_confirmation')
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mb-6">
+            <button type="submit" name="submit" value="edit"
+                    class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500 mr-4"
+            >
+                Submit
+            </button>
+
+            <a href="{{ $user->path() }}" class="hover:underline">Cancel</a>
+        </div>
+    </form>
+</x-app>
