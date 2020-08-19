9c24e9ef43eeb607851520d3c048abe0c0d9b30c
Foldesi Peter <peter.foldesi@nevis.hu>
2020-08-19 16:55:44 +0200

    Update devdataseeder

--- database/seeds/DevDataSeeder.php
+++ database/seeds/DevDataSeeder.php
@@ -12,10 +12,21 @@ class DevDataSeeder extends Seeder
      */
     public function run()
     {
+        //create two user
+        //then first follows the second
+        //then both tweets something
+
         $elek = factory(User::class)->create([
             'name' => 'Test Elek',
             'username' => 'testelek',
             'email' => 'test@test1.com',
         ]);
+
+        $newUser = factory(User::class)->create();
+
+        $elek->follow($newUser);
+
+        factory('App\Tweet')->create(['user_id'=>$elek->id]);
+        factory('App\Tweet')->create(['user_id'=>$newUser->id]);
     }
 }
