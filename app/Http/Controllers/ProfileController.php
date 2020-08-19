aac8b7227ffb8c4a78a3375b1ff40d3eedb01bd7
Foldesi Peter <peter.foldesi@nevis.hu>
2020-08-19 17:07:55 +0200

    Fix ProfileController class name

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Validation\Rule;
 
-class ProfilesController extends Controller
+class ProfileController extends Controller
 {
     /**
      * Get the route key for the model.
